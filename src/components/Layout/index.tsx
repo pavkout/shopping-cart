e07310f3193b2c8beef6a0ca9dc3a3f80c92e5bd
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-08 11:39:37 +0300

    Added header and footer into layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,29 +1,15 @@
-import Link from 'next/link';
+import Header from '../Header';
+import Footer from '../Footer';
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => (
-  <div className="container mx-auto px-4">
-    <div className="flex justify-between">
-      <strong>Qogita</strong>
-      <nav>
-        <ul className="flex gap-4">
-          <li>
-            <Link href="/">
-              <a className="underline">Products</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/cart">
-              <a className="underline">Your Cart</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+  <div className='container mx-auto px-4'>
+    <Header />
     {children}
+    <Footer />
   </div>
 );
 
