5d5f43e5892f17b77f5a3bd12024e9f505a6452d
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-08 15:55:59 +0300

    Fetch products and display them to the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,24 @@
-const HomePage = () => <h1>Products</h1>;
+import { getPage } from './api/products';
+import ProductCard from '../components/ProductCard';
+import { Product } from '../types';
+
+const HomePage = ({ products }: any) => {
+  return (
+    <div className='flex flex-col'>
+      <h1>Products</h1>
+      <div className='grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
+        {products.map((p: Product) => (
+          <ProductCard key={p.gtin} product={p} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
+
+export async function getServerSideProps(ctx: any) {
+  const products = await getPage(1);
+
+  return { props: { products } };
+}
