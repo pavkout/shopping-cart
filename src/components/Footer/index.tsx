ddbe856ada8f787a856cf7f995de0c2f42a516b7
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-08 12:39:04 +0300

    Modify the footer compoent and add some content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,7 @@
-const Footer = () => {
-  return (
-    <div className='flex justify-between'>
-      <span>Footer</span>
-    </div>
-  );
-};
+const Footer = () => (
+  <footer className='pt-4 pb-0 text-black text-center'>
+    Copyright <strong>Qogita</strong> {new Date().getFullYear()}
+  </footer>
+);
 
 export default Footer;
