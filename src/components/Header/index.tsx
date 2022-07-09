a693de5509a058f9db367ba68ca97a82424109ba
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-09 16:57:16 +0300

    Use the new Nav component and the banner component into header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,13 @@
-import Link from 'next/link';
+import Banner from '../Banner';
+import Nav from '../Nav';
 
-const Header = () => {
+function Header() {
   return (
-    <div className='flex justify-between'>
-      <strong>Qogita</strong>
-      <nav>
-        <ul className='flex gap-4'>
-          <li>
-            <Link href='/'>
-              <a className='underline'>Products</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/cart'>
-              <a className='underline'>Your Cart</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className='absolute w-full z-10'>
+      <Banner />
+      <Nav />
     </div>
   );
-};
+}
 
 export default Header;
