b1ef4a8a471d7fec8e945d172b2ce84c8398f07c
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-09 16:56:04 +0300

    Added the background image component and remove content from Layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,16 @@
 import Header from '../Header';
-import Content from '../Content';
 import Footer from '../Footer';
+import HeroImage from '../HeroImage';
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => (
-  <div className='container mx-auto px-4'>
+  <div className='h-screen relative'>
     <Header />
-    <Content>{children}</Content>
+    <HeroImage />
+    {children}
     <Footer />
   </div>
 );
