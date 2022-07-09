489eac40f2fc1692378184eb3560ec76cdcbccaa
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-09 16:55:27 +0300

    Create a background Image component

---
+++ src/components/HeroImage/index.tsx
@@ -0,0 +1,12 @@
+const HeroImage = () => {
+  return (
+    <div
+      className='bg-center bg-cover bg-fixed bg-no-repeat md:w-full h-3/4'
+      style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)), url('https://us.123rf.com/450wm/siciliart/siciliart1712/siciliart171200263/92178360-new-collection-fashion-header-gold-elegant-frame-with-artistic-hand-drawn-brush-texture-in-pastel-gr.jpg?ver=6')`,
+      }}
+    />
+  );
+};
+
+export default HeroImage;
