8000218de2172927addd8d5a9c290eed4baec771
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-10 15:57:03 +0300

    Added types for state management

---
+++ src/state/types.tsx
@@ -0,0 +1,6 @@
+// Types for action creators
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const INCREASE_CART_QUANTITY = 'INCREASE_CART_QUANTITY';
+export const SUBTRACT_CART_QUANTITY = 'SUBTRACT_CART_QUANTITY';
+export const RESET_CART = 'RESET_CART';
