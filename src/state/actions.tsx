c5432b9fff36c53dfd748537256ae7955d28ecae
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-10 15:59:12 +0300

    Create actions for state management

---
+++ src/state/actions.tsx
@@ -0,0 +1,32 @@
+import { Product } from '../types';
+import {
+  ADD_TO_CART,
+  INCREASE_CART_QUANTITY,
+  REMOVE_FROM_CART,
+  RESET_CART,
+  SUBTRACT_CART_QUANTITY,
+} from './types';
+
+export const addToCart = (product: Product) => ({
+  type: ADD_TO_CART,
+  payload: product,
+});
+
+export const removeFromCart = (gtin: string) => ({
+  type: REMOVE_FROM_CART,
+  payload: gtin,
+});
+
+export const increaseCartQuantity = (gtin: string) => ({
+  type: INCREASE_CART_QUANTITY,
+  payload: gtin,
+});
+
+export const subtractCartQuantity = (gtin: string) => ({
+  type: SUBTRACT_CART_QUANTITY,
+  payload: gtin,
+});
+
+export const resetCart = () => ({
+  type: RESET_CART,
+});
