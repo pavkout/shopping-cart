96ec778226f6440fc36e75aa2d0d1cdbd96a39e1
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-10 19:15:48 +0300

    Create an Empty cart component to display it when cart is empty

---
+++ src/components/ShoppingCart/EmptyCart.tsx
@@ -0,0 +1,18 @@
+const EmptyCart = () => {
+  return (
+    <div className='flex flex-col items-center justify-center text-center pt-20'>
+      <img
+        src='https://i.imgur.com/dCdflKN.png'
+        width='80'
+        height='80'
+        className='img-fluid'
+      />
+      <h3>
+        <strong>Your Cart is Empty</strong>
+      </h3>
+      <h4>Add something to make me happy :)</h4>
+    </div>
+  );
+};
+
+export default EmptyCart;
