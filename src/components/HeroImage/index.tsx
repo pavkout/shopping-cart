c124ab282b566e4d33732cd91087a7d4f73bec72
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-10 22:27:04 +0300

    Change the bg image

--- src/components/HeroImage/index.tsx
+++ src/components/HeroImage/index.tsx
@@ -1,9 +1,9 @@
 const HeroImage = () => {
   return (
     <div
-      className='bg-center bg-cover bg-fixed bg-no-repeat md:w-full h-3/4'
+      className='bg-center bg-cover bg-fixed bg-purple-300 bg-no-repeat md:w-full h-3/4'
       style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)), url('https://us.123rf.com/450wm/siciliart/siciliart1712/siciliart171200263/92178360-new-collection-fashion-header-gold-elegant-frame-with-artistic-hand-drawn-brush-texture-in-pastel-gr.jpg?ver=6')`,
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)), url('https://pngimage.net/wp-content/uploads/2018/06/png-new-collection-1.png')`,
       }}
     />
   );
