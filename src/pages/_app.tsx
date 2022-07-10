f42b7499ab0e9030780871225f4d3d55182e765e
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-10 18:16:07 +0300

    Added ToastProvider into _app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { ToastProvider } from 'react-toast-notifications';
 
 import Layout from '../components/Layout';
 import { Store } from '../state/store';
@@ -6,11 +7,13 @@ import { Store } from '../state/store';
 import '../global.css';
 
 const QogitaApp = ({ Component, pageProps }: AppProps): JSX.Element => (
-  <Store>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  </Store>
+  <ToastProvider autoDismiss={true} placement='bottom-left'>
+    <Store>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Store>
+  </ToastProvider>
 );
 
 export default QogitaApp;
