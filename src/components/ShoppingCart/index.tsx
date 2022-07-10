a7fa78aaeb61580836d9ab62bcacab9dd123b8d4
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-10 19:30:18 +0300

    Added the new components to shopping cart component

--- src/components/ShoppingCart/index.tsx
+++ src/components/ShoppingCart/index.tsx
@@ -1,14 +1,10 @@
 import { useRouter } from 'next/router';
-import { Fragment, useContext } from 'react';
+import { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { XIcon } from '@heroicons/react/outline';
-import { useToasts } from 'react-toast-notifications';
 
-import ShoppingList from './ShoppingList';
-
-import { ShoppingContext } from '../../state/store';
-import { formatPrice } from '../../utils';
-import { resetCart } from '../../state/actions';
+import Header from './Header';
+import ProductList from './ProductList';
+import Footer from './Footer';
 
 type Props = {
   open: boolean;
@@ -17,28 +13,13 @@ type Props = {
 const ShoppingCart = ({ open }: Props) => {
   // Use router object
   const router = useRouter();
-  // Use context
-  const { state, dispatch } = useContext(ShoppingContext);
-  // Use toast system.
-  const { addToast } = useToasts();
-
-  const { cart, totalPrice, totalItems } = state;
-
-  const handleEraseCart = () => {
-    dispatch(resetCart());
-    addToast('The cart has successfully erased.', {
-      appearance: 'success',
-    });
-  };
 
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
         as='div'
         className='relative z-10'
-        onClose={() => {
-          router.push('/');
-        }}
+        onClose={() => router.push('/')}
       >
         <Transition.Child
           as={Fragment}
@@ -67,68 +48,11 @@ const ShoppingCart = ({ open }: Props) => {
                 <Dialog.Panel className='pointer-events-auto w-screen max-w-md'>
                   <div className='flex h-full flex-col overflow-y-scroll bg-white shadow-xl'>
                     <div className='flex-1 overflow-y-auto py-6 px-4 sm:px-6'>
-                      <div className='flex items-start justify-between'>
-                        <Dialog.Title className='text-lg font-medium text-gray-900'>
-                          {' '}
-                          Shopping cart{' '}
-                        </Dialog.Title>
-                        <div className='ml-3 flex h-7 items-center'>
-                          <button
-                            type='button'
-                            className='-m-2 p-2 text-gray-400 hover:text-gray-500'
-                            onClick={() => router.push('/')}
-                          >
-                            <span className='sr-only'>Close panel</span>
-                            <XIcon className='h-6 w-6' aria-hidden='true' />
-                          </button>
-                        </div>
-                      </div>
-
-                      <ShoppingList />
+                      <Header />
+                      <ProductList />
                     </div>
 
-                    <div className='border-t border-gray-200 py-6 px-4 sm:px-6'>
-                      <div className='flex justify-between text-base font-medium text-gray-900'>
-                        <p>Subtotal</p>
-                        <p>{formatPrice('€', totalPrice)}</p>
-                      </div>
-                      <p className='mt-0.5 text-sm text-gray-500'>
-                        Shipping and taxes calculated at checkout.
-                      </p>
-                      <div className='mt-6'>
-                        <button
-                          onClick={() => router.push('/')}
-                          className='flex items-center justify-center w-full rounded-md border border-transparent bg-purple-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-purple-700'
-                        >
-                          Checkout
-                        </button>
-                      </div>
-                      <div className='mt-6 flex justify-center text-center text-sm text-gray-500'>
-                        <p>
-                          {totalItems > 0 && (
-                            <>
-                              or{' '}
-                              <button
-                                type='button'
-                                className='font-medium text-gray-400 hover:text-gray-500'
-                                onClick={handleEraseCart}
-                              >
-                                Erase Cart
-                              </button>{' '}
-                            </>
-                          )}
-                          or{' '}
-                          <button
-                            type='button'
-                            className='font-medium text-purple-600 hover:text-purple-500'
-                            onClick={() => router.push('/')}
-                          >
-                            Continue Shopping
-                            <span aria-hidden='true'> &rarr;</span>
-                          </button>
-                        </p>
-                      </div>
-                    </div>
+                    <Footer />
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
