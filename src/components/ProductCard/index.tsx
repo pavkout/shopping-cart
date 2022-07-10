cad753f08aeab822d6e55396bf4f18a2e15699f2
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-10 18:13:58 +0300

    Added functionality like new toast system and formatPrice into product card

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -2,27 +2,46 @@ import { useContext } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ShoppingCartIcon } from '@heroicons/react/outline';
+import { useToasts } from 'react-toast-notifications';
 
 import { Product } from '../../types';
 import { ShoppingContext } from '../../state/store';
 import { addToCart } from '../../state/actions';
+import { formatPrice } from '../../utils';
 
 type Props = {
   product: Product;
 };
 
 const ProductCard = ({ product }: Props) => {
+  // Destructure needed props.
+  const {
+    gtin,
+    name,
+    imageUrl,
+    recommendedRetailPrice,
+    recommendedRetailPriceCurrency,
+  } = product;
+
   // Use context
   const { dispatch } = useContext(ShoppingContext);
 
+  // Use toast system.
+  const { addToast } = useToasts();
+
+  // This function fires when user click add to cart button.
   const handleAddClick = () => {
     dispatch(addToCart({ ...product, quantity: 1 }));
+    addToast(`${name} has successfully added to cart.`, {
+      appearance: 'success',
+    });
   };
+
   return (
     <div className='group relative overflow-hidden rounded-md px-4 pt-4 pb-20 shadow-lg hover:cursor-pointer'>
       <figure className='flex justify-center items-center px-10 '>
         <Image
-          src={product.imageUrl}
+          src={imageUrl}
           alt='Product Image'
           className='object-contain w-full h-64'
           height={300}
@@ -30,15 +49,14 @@ const ProductCard = ({ product }: Props) => {
         />
       </figure>
       <div className='flex justify-between'>
-        <Link href={`/?gtin=${product.gtin}`} as={`/product/${product.gtin}`}>
+        <Link href={`/?gtin=${gtin}`} as={`/product/${gtin}`}>
           <h2 className='text-md text-gray-700 pr-4'>
             <span aria-hidden='true' className='absolute inset-0' />
-            {product.name}
+            {name}
           </h2>
         </Link>
         <p className='text-md font-medium text-gray-900'>
-          {product.recommendedRetailPrice}
-          {product.recommendedRetailPriceCurrency}
+          {formatPrice(recommendedRetailPriceCurrency, recommendedRetailPrice)}
         </p>
       </div>
       <button
