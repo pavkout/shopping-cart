59d847870cc876746d1edba456f05a62aacdf97c
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-11 21:48:18 +0300

    Make the elert modal dark friedly

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -45,8 +45,8 @@ const Alert = ({ open, title, description, onConfirm, onReject }: Props) => {
               leaveFrom='opacity-100 translate-y-0 sm:scale-100'
               leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
-              <Dialog.Panel className='relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full'>
-                <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
+              <Dialog.Panel className='relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full dark:bg-gray-900'>
+                <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 dark:bg-gray-900'>
                   <div className='sm:flex sm:items-start'>
                     <div className='mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10'>
                       <ExclamationIcon
@@ -57,7 +57,7 @@ const Alert = ({ open, title, description, onConfirm, onReject }: Props) => {
                     <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
                       <Dialog.Title
                         as='h3'
-                        className='text-lg leading-6 font-medium text-gray-900'
+                        className='text-lg leading-6 font-medium text-gray-900 dark:text-gray-200'
                       >
                         {title}
                       </Dialog.Title>
@@ -67,7 +67,7 @@ const Alert = ({ open, title, description, onConfirm, onReject }: Props) => {
                     </div>
                   </div>
                 </div>
-                <div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
+                <div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse dark:bg-gray-900'>
                   <button
                     type='button'
                     className='w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm'
