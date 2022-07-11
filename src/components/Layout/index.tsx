c99d8c23bbca449a723fe7ed93651093dc366b7e
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-11 19:52:25 +0300

    Add ThemeSwitcher into Layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,16 +1,18 @@
 import Header from '../Header';
 import Footer from '../Footer';
 import HeroImage from '../HeroImage';
+import ThemeSwitcher from '../ThemeSwitcher';
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = ({ children }: Props) => (
-  <div className='h-screen relative'>
+  <div className='h-screen relative dark:bg-gray-900'>
     <Header />
     <HeroImage />
     {children}
+    <ThemeSwitcher />
     <Footer />
   </div>
 );
