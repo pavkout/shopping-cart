9d4699bd63b80c03fec3ce6413d655c9df6c0a38
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-11 19:51:09 +0300

    Change bg image of heroImage

--- src/components/HeroImage/index.tsx
+++ src/components/HeroImage/index.tsx
@@ -3,7 +3,7 @@ const HeroImage = () => {
     <div
       className='bg-center bg-cover bg-fixed bg-purple-300 bg-no-repeat md:w-full h-3/4'
       style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)), url('https://pngimage.net/wp-content/uploads/2018/06/png-new-collection-1.png')`,
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.3)), url('https://cdn.shopify.com/s/files/1/0191/6390/collections/banner_new-arrivals_1200x1600.jpg?v=1586155787')`,
       }}
     />
   );
