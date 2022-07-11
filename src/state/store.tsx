11a309e78447b8872dd91dc19a6040803ec27ce6
Pavlos Koutoglou <pkoutoglou@gmail.com>
2022-07-11 21:18:01 +0300

    Added functionality to save cart in localstorage in order to have it any time

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -3,11 +3,14 @@ import React, {
   ReactElement,
   ReactNode,
   useContext,
+  useEffect,
+  useMemo,
   useReducer,
 } from 'react';
 import Reducer from './reducer';
 import { ContextType } from '../types';
 import { initialState } from './initialState';
+import { initCart } from './actions';
 
 /**
  * React Context-based Global Store with a reducer
@@ -15,8 +18,29 @@ import { initialState } from './initialState';
 export function Store({ children }: { children: ReactNode }): ReactElement {
   const [state, dispatch] = useReducer(Reducer, initialState);
 
+  const contextValue = useMemo(() => {
+    return { state, dispatch };
+  }, [state, dispatch]);
+
+  useEffect(() => {
+    // Get from local storage by key
+    const item = window.localStorage.getItem('state');
+
+    if (item && JSON.parse(item)) {
+      // Checking if there already is a state in localstorage, if yes, update the current state with the stored one
+      dispatch(initCart(JSON.parse(item)));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (state !== initialState) {
+      // Create and/or set a new localstorage variable called "state"
+      localStorage.setItem('state', JSON.stringify(state));
+    }
+  }, [state]);
+
   return (
-    <ShoppingContext.Provider value={{ state, dispatch }}>
+    <ShoppingContext.Provider value={contextValue}>
       {children}
     </ShoppingContext.Provider>
   );
